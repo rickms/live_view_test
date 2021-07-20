04890019ced910e3dc6f6833936294e54df448fa
Rick Smorawski <rickms@gmail.com>
2021-07-19 19:46:54 -0500

    remove logging

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,10 @@ use actix_files::{self as fs};
 use actix_web::*;
 
 use crate::app::AppState;
-use actix_web::middleware::Logger;
-use env_logger::Env;
 use actix_cors::Cors;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
 
     let app_state = web::Data::new(AppState::default());
     HttpServer::new(move || {
@@ -26,7 +22,6 @@ async fn main() -> std::io::Result<()> {
 
         App::new()
             .wrap(cors)
-            .wrap(Logger::default())
             .app_data(app_state.clone())
             .service(services::todo::get_todos)
             .service(services::todo::create_todo)
